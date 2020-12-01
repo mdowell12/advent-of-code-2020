8435a4b9627eeb2bbd8ccd57148339271684ef1b
Matt Dowell <mattdowell12@gmail.com>
2020-12-01 06:59:35 -0500

    Finish day 1

--- solutions/get_inputs.py
+++ solutions/get_inputs.py
@@ -7,7 +7,7 @@ import requests
 SESSION_COOKIE = '_ga=GA1.2.1645853937.1605627450; session=53616c7465645f5fe567cd8537a15049458afbc753e621f14f19b476db94711675493b55592e42737eabaccc62511057; _gid=GA1.2.1874702779.1606821314; _gat=1'
 
 URL = "https://adventofcode.com/2020/day/%s/input"
-BASE_DIR = os.path.join(os.path.dirname(__file__), "./inputs")
+BASE_DIR = os.path.join(os.path.dirname(__file__), "../inputs")
 
 
 def run():
@@ -26,6 +26,12 @@ def run():
     cache_location = cache_inputs(day, inputs)
     print("Wrote inputs to location " + cache_location)
     return 0
+
+
+def read_inputs(day):
+    day_file = get_day_file(day)
+    with open(day_file, 'r') as f:
+        return [i for i in f.read().strip().split('\n')]
 
 
 def is_cached(day):
