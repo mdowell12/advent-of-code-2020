d1414b7dd1abe72a709efafc79596aa61ccaab2f
Fernando Seror <ferdy89@gmail.com>
2020-12-01 19:08:03 -0800

    Part 2 of Fer's solution for day 1

--- solutions_fer/day1/src/main.rs
+++ solutions_fer/day1/src/main.rs
@@ -4,17 +4,21 @@ use std::iter::FromIterator;
 
 fn main() {
     let stdin = io::stdin();
-    let number_set: HashSet<i32> = HashSet::from_iter(
-        stdin
-            .lock()
-            .lines()
-            .map(|elem| elem.unwrap().parse::<i32>().unwrap()),
-    );
+    let numbers = stdin
+        .lock()
+        .lines()
+        .map(|elem| elem.unwrap().parse::<i32>().unwrap());
 
-    let number: &i32 = number_set
-        .iter()
-        .find(|num| number_set.contains(&(2020 - *num)))
-        .unwrap();
+    let number_set: HashSet<i32> = HashSet::from_iter(numbers);
 
-    println!("{}", number * (2020 - number));
+    for num1 in number_set.iter() {
+        for num2 in number_set.iter() {
+            let complimentary = 2020 - num1 - num2;
+
+            if number_set.contains(&complimentary) {
+                println!("{}", num1 * num2 * complimentary);
+                return;
+            }
+        }
+    }
 }
