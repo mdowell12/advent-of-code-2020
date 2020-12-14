edf0a3ae8a91d1effde9385fad95ea8359b7b79e
Fernando Seror <ferdy89@gmail.com>
2020-12-13 16:32:26 -0800

    Fer's solution to day 13

---
+++ solutions_fer/day13/src/main.rs
@@ -0,0 +1,100 @@
+use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::io::{self, BufRead};
+
+struct Bus {
+    id: usize,
+    offset: usize,
+}
+
+impl Bus {
+    pub fn matches_timestamp(&self, timestamp: usize) -> bool {
+        let res = (timestamp + self.offset) % self.id == 0;
+        if res {
+            println!(
+                "(found ({} + {}) == {} * {})",
+                timestamp,
+                self.offset,
+                self.id,
+                (timestamp + self.offset) / self.id
+            )
+        }
+        res
+    }
+}
+
+struct BusDepartures {
+    current: usize,
+    step: usize,
+}
+
+impl Iterator for BusDepartures {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        let current = self.current;
+
+        self.current += self.step;
+
+        Some(current) // It'll never be None, but the Iterator trait requires this
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines_iter = stdin.lock().lines();
+    let timestamp = lines_iter
+        .next()
+        .unwrap()
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+    let all_buses_line = lines_iter.next().unwrap().unwrap();
+    let all_buses = all_buses_line.split(",");
+    let mut buses: Vec<Bus> = all_buses
+        .enumerate()
+        .filter(|(_, bus)| *bus != "x")
+        .map(|(idx, bus)| Bus {
+            id: bus.parse::<usize>().unwrap(),
+            offset: idx,
+        })
+        .collect();
+
+    let mut next_buses = HashMap::<usize, usize>::new();
+
+    for bus in buses.iter() {
+        let times = (timestamp as f32 / bus.id as f32).ceil() as usize;
+        next_buses.insert(times * bus.id, bus.id);
+    }
+    let next_bus = next_buses.keys().min().unwrap();
+
+    println!(
+        "Part 1: {}",
+        next_buses.get(next_bus).unwrap() * (next_bus - timestamp)
+    );
+
+    buses.sort_by_key(|bus| isize::try_from(bus.id).unwrap() * -1);
+
+    let mut current_candidate = 1;
+    let mut current_step = 1;
+    for bus in buses.iter() {
+        let mut bus_departures = BusDepartures {
+            current: current_candidate,
+            step: current_step,
+        };
+        current_candidate = bus_departures
+            .find(|dep| bus.matches_timestamp(*dep))
+            .unwrap();
+        let next_matching_departure = bus_departures
+            .find(|dep| bus.matches_timestamp(*dep))
+            .unwrap();
+        current_step = next_matching_departure - current_candidate;
+        println!(
+            "==> Candidate for bus {} with offset {}: {}. Next valid candidates in steps of: {}
+            ",
+            bus.id, bus.offset, current_candidate, current_step
+        );
+    }
+
+    println!("Part 2: {}", current_candidate);
+}
