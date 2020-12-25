89c806e6b6cbbd7b52109b1396465c011dff1895
Matt Dowell <mattdowell12@gmail.com>
2020-12-25 17:08:41 -0500

    Day 24

--- solutions/24/run.py
+++ solutions/24/run.py
@@ -3,42 +3,123 @@ from collections import defaultdict
 from solutions.get_inputs import read_inputs
 
 
+class Tile:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def move(self, directions):
+        for direction in directions:
+            if direction == 'e':
+                self.x += 2
+            elif direction == 'w':
+                self.x += -2
+            elif direction == 'ne':
+                self.x += 1
+                self.y += 1
+            elif direction == 'nw':
+                self.x += -1
+                self.y += 1
+            elif direction == 'se':
+                self.x += 1
+                self.y += -1
+            elif direction == 'sw':
+                self.x += -1
+                self.y += -1
+            else:
+                Exception(direction)
+        return self
+
+    def position(self):
+        return (self.x, self.y)
+
+    @staticmethod
+    def adjacents(x, y):
+        return [
+            (x + 2, y),
+            (x - 2, y),
+            (x + 1, y + 1),
+            (x - 1, y + 1),
+            (x + 1, y - 1),
+            (x - 1, y - 1),
+        ]
+
+    @staticmethod
+    def num_adjacent_black(x, y, grid):
+        neighbors = Tile.adjacents(x, y)
+        num_black = 0
+        for neighbor in neighbors:
+            if grid[neighbor] == -1:
+                num_black += 1
+        return num_black
+
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+
 def run_1(inputs):
     raw_directions = [parse_raw(i) for i in inputs]
-    normalized = [normalize(i) for i in raw_directions]
-    import pdb; pdb.set_trace()
-    tiles_flipped = defaultdict(lambda: 0)
-    for tile in normalized:
-        tiles_flipped[tile] += 1
+    tiles = [Tile().move(i) for i in raw_directions]
+    grid = defaultdict(lambda: 1)
+    for tile in tiles:
+        grid[tile.position()] *= -1
 
-    return len([tile for tile, times_flipped in tiles_flipped.items() if times_flipped % 2 != 0])
+    return len([tile for tile, value in grid.items() if value == -1])
 
 
 
 def run_2(inputs):
-    pass
+    raw_directions = [parse_raw(i) for i in inputs]
+    tiles = [Tile().move(i) for i in raw_directions]
+    grid = defaultdict(lambda: 1)
+    for tile in tiles:
+        grid[tile.position()] *= -1
+    print_grid(grid)
+    for i in range(100):
+        # Copy grid since we will mutate it
+        new_grid = defaultdict(lambda: 1)
+        for position, value in grid.items():
+            new_grid[position] = value
+
+        black_tiles = [tile for tile, value in grid.items() if value == -1]
+        tiles_to_check = set(black_tiles).union(set(tile for x, y in black_tiles for tile in Tile.adjacents(x, y)))
+        for position in tiles_to_check:
+            x, y = position
+            value = grid[position]
+            if value == 1:
+                # while tile
+                if Tile.num_adjacent_black(x, y, grid) == 2:
+                    new_grid[position] = -1
+            else:
+                # black tile
+                if Tile.num_adjacent_black(x, y, grid) in (0, 3, 4, 5, 6):
+                    new_grid[position] = 1
+
+        grid = defaultdict(lambda: 1)
+        for position, value in new_grid.items():
+            grid[position] = value
+
+        print(i, len([tile for tile, value in grid.items() if value == -1]))
+        print_grid(grid)
+
+    return len([tile for tile, value in grid.items() if value == -1])
 
 
-def normalize(directions):
-    counts = defaultdict(lambda: 0)
-    for direction in directions:
-        counts[direction] += 1
-
-    counts['e'] = counts['e'] - counts['w']
-    counts['w'] = 0
-    counts['ne'] = counts['ne'] - counts['sw']
-    counts['sw'] = 0
-    counts['nw'] = counts['nw'] - counts['se']
-    counts['se'] = 0
-    print(counts)
-    normalized = ''
-    normalized += 'e' * counts['e'] if counts['e'] > 0 else 'w' * (-1 * counts['e'])
-    normalized += 'ne' * counts['ne'] if counts['ne'] > 0 else 'sw' * (-1 * counts['ne'])
-    normalized += 'nw' * counts['nw'] if counts['nw'] > 0 else 'se' * (-1 * counts['nw'])
-    # if counts['ne'] < 0:
-    #     import pdb; pdb.set_trace()
-    return normalized
-
+def print_grid(grid):
+    max_x = max(abs(i) for i, _ in grid)
+    max_y = max(abs(i) for _, i in grid)
+    rows = []
+    for j in range(max_y+1, -max_y, -1):
+        row = ''
+        for col in range(-max_x, max_x+1):
+            if j == col == 0:
+                row += 'X' if grid[(col, j)] == -1 else 'O'
+            else:
+                row += 'x' if grid[(col, j)] == -1 else '.'
+        rows.append(row)
+    print('\n'.join(rows))
 
 
 def parse_raw(line):
@@ -81,9 +162,9 @@ def run_tests():
     if result_1 != 10:
         raise Exception(f"Test 1 did not past, got {result_1}")
 
-    # result_2 = run_2(test_inputs)
-    # if result_2 != 0:
-    #     raise Exception(f"Test 2 did not past, got {result_2}")
+    result_2 = run_2(test_inputs)
+    if result_2 != 2208:
+        raise Exception(f"Test 2 did not past, got {result_2}")
 
 
 if __name__ == "__main__":
@@ -92,9 +173,7 @@ if __name__ == "__main__":
     input = read_inputs(24)
 
     result_1 = run_1(input)
-    # 151 is too low
-    # 451 is too high
     print(f"Finished 1 with result {result_1}")
 
-    # result_2 = run_2(input)
-    # print(f"Finished 2 with result {result_2}")
+    result_2 = run_2(input)
+    print(f"Finished 2 with result {result_2}")
